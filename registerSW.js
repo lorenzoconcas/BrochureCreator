5d391911a6945b477902d1d3521c01b97cb879fa
Lorenzo Concas <35010999+lorenzoconcas@users.noreply.github.com>
2024-10-30 17:06:31 +0100

    fix

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if ('serviceWorker' in navigator) { window.addEventListener('load', () => { navigator.serviceWorker.register('sw.js', { scope: '/BrochureCreator/' }) }) }
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BrochureCreator/sw.js', { scope: '/BrochureCreator/' })})}
