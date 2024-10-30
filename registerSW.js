94076543a6c8618a795072ac1b2706f91f4cc9ab
Lorenzo Concas <35010999+lorenzoconcas@users.noreply.github.com>
2024-10-30 16:22:47 +0100

    Update index.html
    Update index.html
    
    Update index.html
    
    Update index.html
    
    Update index.html
    
    Update registerSW.js
    
    Update manifest.webmanifest
    
    Update index.html

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if ('serviceWorker' in navigator) { window.addEventListener('load', () => { navigator.serviceWorker.register('sw.js', { scope: 'https://lorenzoconcas.github.io/BrochureCreator/' }) }) }
